af241b121ce7705ecba19b12a4ee36d1e1c402a1
Roman <tvgs-krv@mail.ru>
2021-12-09 13:10:49 +0300

    [add] new icons to IconBar

--- src/components/LeftIconBar/LeftIconBar.js
+++ src/components/LeftIconBar/LeftIconBar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import s from './LeftIconBar.module.scss';
+import {ReactComponent as InitialData} from "../../Images/Svg/initial_data_icon.svg";
+import {ReactComponent as DiagramIcon} from "../../Images/Svg/diagram_icon.svg";
 import {ReactComponent as Icon1} from "../../Images/Svg/angle-double-down-solid.svg";
 import {ReactComponent as Icon2} from "../../Images/Svg/car-alt-solid.svg";
 
@@ -7,6 +9,16 @@ function LeftIconBar()
 {
     return (
         <div className={s.left_bar}>
+            <div className={s.container}>
+                <a className={s.center_btn} href="#">
+                    <InitialData className={s.svg_fill}/>
+                </a>
+            </div>
+            <div className={s.container}>
+                <a className={s.center_btn} href="#">
+                    <DiagramIcon className={s.svg_fill}/>
+                </a>
+            </div>
             <div className={s.container}>
                 <a className={s.center_btn} href="#">
                     <Icon1 className={s.svg_fill}/>
