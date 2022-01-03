72b3d186f533cd702873d51646834db74ca96496
Roman <tvgs-krv@mail.ru>
2022-01-03 23:55:50 +0300

    [add] hooks

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,21 +1,28 @@
 import React, {useEffect, useRef, useState} from "react";
 import s from "./Profile.module.scss"
-import {ReactComponent as UserLogo} from "../../Images/Svg/user-circle-solid.svg";
-import {Link} from "react-router-dom";
 
 
 const Profile = () => {
-    const imgUrl = "https://mir-s3-cdn-cf.behance.net/project_modules/fs/8c8ecf23475771.56323fdddcb3d.jpg"
+
+    const data = {
+        id: 1,
+        name: "Зинаид Евкакьевич",
+        image:{
+            url: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/8c8ecf23475771.56323fdddcb3d.jpg",
+            alt: "Ваше фото"
+        }
+    }
+    const [getData, setData] = useState(data);
+
     return (
         <div>
             <div className={s.profile_sidebar}>
-                <div className={s.photo}><img src={imgUrl} alt="Ваше фото"/></div>
-                <div className={s.name}><span>ЗИНАИД ЕВКАКЬЕВИЧ</span></div>
+                <div className={s.photo}><img src={getData.image.url} alt={getData.image.alt}/></div>
+                <div className={s.name}><span>{getData.name.toUpperCase()}</span></div>
                 <div className={s.profile_link}><a href="#">Проекты</a></div>
                 <div className={s.profile_link}><a href="#">Настройки</a></div>
                 <div className={s.profile_link}><a href="#">Крендельки</a></div>
             </div>
-
             <div className={s.profile_content}>
                 Content
             </div>
