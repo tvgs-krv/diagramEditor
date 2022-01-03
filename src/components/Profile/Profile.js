0ea4c259dda5c86da18a9913e4967406b97b669a
Roman <tvgs-krv@mail.ru>
2022-01-03 21:00:56 +0300

    [add] Profile page

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useRef, useState} from "react";
+
+
+const Profile = () => {
+    return (
+        <div>
+            <div>
+                Profile
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
