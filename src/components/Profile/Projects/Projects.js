569cbd705843eb3bad999fa327168a68772ef4b8
Roman <tvgs-krv@mail.ru>
2022-01-10 10:06:42 +0300

    [add] project module

---
+++ src/components/Profile/Projects/Projects.js
@@ -0,0 +1,10 @@
+import React, {useEffect, useRef, useState} from "react";
+import s from "./Projects.module.scss"
+
+const Profile = ({projects}) => {
+    return(
+        <div>
+
+        </div>
+    )
+}
