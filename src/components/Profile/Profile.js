209214be090dd3e65439eeb0c6098897681a37aa
Roman <tvgs-krv@mail.ru>
2022-01-10 10:07:32 +0300

    [upd] state

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,10 +7,28 @@ const Profile = () => {
     const data = {
         id: 1,
         name: "Зинаид Евкакьевич",
-        image:{
+        image: {
             url: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/8c8ecf23475771.56323fdddcb3d.jpg",
             alt: "Ваше фото"
-        }
+        },
+        "projects": [
+            {
+                "id": 1,
+                "type": "diagram",
+                "name": "test1",
+                "creationDate": "04.04.2022",
+                "modifiedDate": "04.04.2022",
+                "description": "Описание проекта тест1"
+            },
+            {
+                "id": 2,
+                "type": "diagram",
+                "name": "test2",
+                "creationDate": "04.04.2022",
+                "modifiedDate": "04.04.2022",
+                "description": "Описание проекта тест2"
+            }
+        ]
     }
     const [getData, setData] = useState(data);
 
@@ -19,11 +37,12 @@ const Profile = () => {
             <div className={s.profile_sidebar}>
                 <div className={s.photo}><img src={getData.image.url} alt={getData.image.alt}/></div>
                 <div className={s.name}><span>{getData.name.toUpperCase()}</span></div>
+                <div className={s.profile_link}><a href="#">Сервисы</a></div>
                 <div className={s.profile_link}><a href="#">Проекты</a></div>
                 <div className={s.profile_link}><a href="#">Настройки</a></div>
                 <div className={s.profile_link}><a href="#">Крендельки</a></div>
             </div>
-            <div className={s.profile_content}>
+            <div className={s.profile_content} projects = {getData.projects}>
                 Content
             </div>
         </div>
